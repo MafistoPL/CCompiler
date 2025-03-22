857c19e167347548da906ed5b08da28db84127e0
MafistoPL <mateusz.poreba@outlook.com>
2025-03-22 20:09:40 +0100

    feat(compiler): add token and position structures for lexical analysis
    - Introduced `struct token` to represent various token types used in lexical analysis.
    - Added token type enum: identifiers, keywords, operators, symbols, etc.
    - Added `struct pos` to track line, column, and filename of code elements.
    - Included flags for whitespace tracking and bracket context.

--- compiler.h
+++ compiler.h
@@ -2,6 +2,48 @@
 #define _COMPILER_H
 
 #include <stdio.h>
+#include <stdbool.h>
+
+struct pos
+{
+    int line;
+    int col;
+    const char *filename;
+};
+
+enum
+{
+    TOKEN_TYPE_IDENTIFIER,
+    TOKEN_TYPE_KEYWORD,
+    TOKEN_TYPE_OPERATOR,
+    TOKEN_TYPE_SYMBOL,
+    TOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_STRING,
+    TOKEN_TYPE_COMMENT,
+    TOKEN_TYPE_NEWLINE
+};
+
+struct token
+{
+    int type;
+    int flags;
+
+    union
+    {
+        char cval;
+        const char *sval;
+        unsigned int lnum;
+        unsigned int llnum;
+        void *any;
+    };
+
+    // True if there is whitespace between the token and the next token
+    // i.e * a for operator token * woudle mean whitespace would be set for token "a"
+    bool whitespace;
+
+    // (5+10+20) for such a expression between_brackets would point to (
+    const char *between_brackets;
+};
 
 enum
 {
